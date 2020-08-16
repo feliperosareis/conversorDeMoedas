e91d4362f66a1f4fe4fd763441c80e7ae452dd68
Felipe Rosa Reis <felipe.reis@leadforce.com.br>
2020-08-15 22:57:32 -0300

    Convesor de Moedas Real/Dolar/Euro.

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace ConversorDeMoedas
+{
+    partial class frmConversor
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCotacao = new System.Windows.Forms.Label();
+            this.txtCotacao = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboPara = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lblCotacao);
+            this.groupBox1.Controls.Add(this.txtCotacao);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtResultado);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtValor);
+            this.groupBox1.Controls.Add(this.btnConverter);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cboPara);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cboDe);
+            this.groupBox1.Location = new System.Drawing.Point(13, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 297);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblCotacao
+            // 
+            this.lblCotacao.AutoSize = true;
+            this.lblCotacao.Location = new System.Drawing.Point(169, 90);
+            this.lblCotacao.Name = "lblCotacao";
+            this.lblCotacao.Size = new System.Drawing.Size(120, 13);
+            this.lblCotacao.TabIndex = 11;
+            this.lblCotacao.Text = "Cotaçãod o Dólar (US$)";
+            // 
+            // txtCotacao
+            // 
+            this.txtCotacao.Enabled = false;
+            this.txtCotacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCotacao.Location = new System.Drawing.Point(169, 106);
+            this.txtCotacao.Name = "txtCotacao";
+            this.txtCotacao.Size = new System.Drawing.Size(134, 26);
+            this.txtCotacao.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(15, 206);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(288, 10);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Resultado:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(15, 254);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(288, 26);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Valor:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(15, 106);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(134, 26);
+            this.txtValor.TabIndex = 0;
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConverter.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConverter.Location = new System.Drawing.Point(15, 154);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(288, 41);
+            this.btnConverter.TabIndex = 2;
+            this.btnConverter.Text = "CONVERTER";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Para:";
+            // 
+            // cboPara
+            // 
+            this.cboPara.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboPara.FormattingEnabled = true;
+            this.cboPara.Items.AddRange(new object[] {
+            "Real (R$)",
+            "Dólar (US$)",
+            "Euro (EU €)"});
+            this.cboPara.Location = new System.Drawing.Point(169, 48);
+            this.cboPara.Name = "cboPara";
+            this.cboPara.Size = new System.Drawing.Size(134, 28);
+            this.cboPara.TabIndex = 4;
+            this.cboPara.Text = "Dólar (US$)";
+            this.cboPara.SelectedIndexChanged += new System.EventHandler(this.cboPara_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "De:";
+            // 
+            // cboDe
+            // 
+            this.cboDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Items.AddRange(new object[] {
+            "Real (R$)",
+            "Dólar (US$)",
+            "Euro (EU €)"});
+            this.cboDe.Location = new System.Drawing.Point(15, 48);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(134, 28);
+            this.cboDe.TabIndex = 3;
+            this.cboDe.Text = "Real (R$)";
+            this.cboDe.SelectedIndexChanged += new System.EventHandler(this.cboDe_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 66);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Heiti Std R", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONVERSOR DE MOEDAS!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Desenvolvido por Felipe Rosa";
+            // 
+            // frmConversor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 407);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmConversor";
+            this.Text = "Conversor de Moedas";
+            this.Load += new System.EventHandler(this.frmConversor_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboPara;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCotacao;
+        private System.Windows.Forms.TextBox txtCotacao;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversorDeMoedas
+{
+    public partial class frmConversor : Form
+    {
+        public frmConversor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            double valor = 0;
+            double cotacao = 0;
+            double resultado = 0;
+            object tipo = cboPara.Text;
+            string tipoDe = cboDe.Text;
+            string tipoPara = cboPara.Text;
+
+
+            if (String.IsNullOrEmpty(txtValor.Text) || Convert.ToDouble(txtValor.Text) == 0)
+            {
+                MessageBox.Show("Informe o valor para conversão de " + tipoDe + " para " + tipoPara);
+            }
+            else
+            {
+                valor = Convert.ToDouble(txtValor.Text);
+            }
+
+
+            if (String.IsNullOrEmpty(txtCotacao.Text) || Convert.ToDouble(txtCotacao.Text) == 0)
+            {
+                MessageBox.Show("Informe a cotação do " + tipo);
+            }
+            else
+            {
+                cotacao = Convert.ToDouble(txtCotacao.Text);
+                resultado = valor / cotacao;
+
+                txtResultado.Text = String.Format("{0:0.00}", resultado);
+                
+            }
+                
+        }
+
+        private void cboDe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            object tipoDe = cboDe.SelectedItem;
+            object tipoPara = cboPara.SelectedItem;
+
+            lblCotacao.Text = "Cotação do " + tipoPara.ToString();
+
+            switch (tipoDe.ToString())
+            {
+                case "Real (R$)":
+                    switch (tipoPara.ToString())
+                    {
+                        case "Dólar (US$)":
+                            txtCotacao.Text = "5,42";
+                            break;
+                        case "Euro (EU €)":
+                            txtCotacao.Text = "6,42";
+                            break;
+                        default:
+                            txtCotacao.Text = "1,00";
+                            break;
+                    }
+                    break;
+                case "Dólar (US$)":
+                    switch (tipoPara.ToString())
+                    {
+                        case "Real (R$)":
+                            txtCotacao.Text = "0,18";
+                            break;
+                        case "Euro (EU €)":
+                            txtCotacao.Text = "1,18";
+                            break;
+                        default:
+                            txtCotacao.Text = "1,00";
+                            break;
+                    }
+                    break;
+                default: //Euro (EU €)
+                    switch (tipoPara.ToString())
+                    {
+                        case "Real (R$)":
+                            txtCotacao.Text = "0,16";
+                            break;
+                        case "Dólar (US$)":
+                            txtCotacao.Text = "0,84";
+                            break;
+                        default:
+                            txtCotacao.Text = "1,00";
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void cboPara_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            object tipoDe = cboDe.SelectedItem;
+            object tipoPara = cboPara.SelectedItem;
+
+            lblCotacao.Text = "Cotação do " + tipoPara.ToString();
+
+            switch (tipoDe.ToString())
+            {
+                case "Real (R$)":
+                    switch (tipoPara.ToString())
+                    {
+                        case "Dólar (US$)":
+                            txtCotacao.Text = "5,42";
+                            break;
+                        case "Euro (EU €)":
+                            txtCotacao.Text = "6,42";
+                            break;
+                        default:
+                            txtCotacao.Text = "1,00";
+                            break;
+                    }
+                    break;
+                case "Dólar (US$)":
+                    switch (tipoPara.ToString())
+                    {
+                        case "Real (R$)":
+                            txtCotacao.Text = "0,18";
+                            break;
+                        case "Euro (EU €)":
+                            txtCotacao.Text = "1,18";
+                            break;
+                        default:
+                            txtCotacao.Text = "1,00";
+                            break;
+                    }
+                    break;
+                default: //Euro (EU €)
+                    switch (tipoPara.ToString())
+                    {
+                        case "Real (R$)":
+                            txtCotacao.Text = "0,16";
+                            break;
+                        case "Dólar (US$)":
+                            if (tipoPara.ToString() == "Dólar (US$)")
+                            {
+                                txtCotacao.Text = "0,84";
+                            }
+                            break;
+                        default:
+                            txtCotacao.Text = "1,00";
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void frmConversor_Load(object sender, EventArgs e)
+        {
+            txtCotacao.Text = "5,42";
+        }
+
+    }
+}
